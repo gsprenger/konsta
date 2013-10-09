3c3006553126dc01102b208cfd714f0259aa115b
Gabriel Sprenger <gabriel.sprenger@gmail.com>
2013-10-09 21:24:54 +0200

    Added the StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Konsta::Application.routes.draw do
+  get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
